01858e33b352bf996926ba463feae7b828248a7d
Mohammad YousefiPour <moyousefipour79@gmail.com>
2021-10-22 08:22:00 +0330

    print Board
    print board of the game.

--- main.py
+++ main.py
@@ -5,9 +5,33 @@ class Item():
 
 
 class Board():
+
     def __init__(self) -> None:
-        item = Item()
-        matrix = []
+        matrix = [
+            [],
+            [],
+            []
+        ]
+        counter = 0
         for i in range(3):
-            matrix.append([item]*3)
+            for j in range(3):
+                counter += 1
+                item = Item()
+                item.value = str(counter)
+
+                matrix[i].append(item)
         self.matrix = matrix
+
+    def print_board(self) -> None:
+        print(self.matrix[0][0].value + '|' +
+              self.matrix[0][1].value + '|' + self.matrix[0][2].value)
+        print('-+-+-')
+        print(self.matrix[1][0].value + '|' +
+              self.matrix[1][1].value + '|' + self.matrix[1][2].value)
+        print('-+-+-')
+        print(self.matrix[2][0].value + '|' +
+              self.matrix[2][1].value + '|' + self.matrix[2][2].value)
+
+
+board = Board()
+board.print_board()
