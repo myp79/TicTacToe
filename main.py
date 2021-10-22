7db8b847b95b672bae2442edc53ffbc9de7a46a6
Mohammad YousefiPour <moyousefipour79@gmail.com>
2021-10-22 08:38:57 +0330

    game function
    game function handle the game.

--- main.py
+++ main.py
@@ -33,5 +33,28 @@ class Board():
               self.matrix[2][1].value + '|' + self.matrix[2][2].value)
 
 
-board = Board()
-board.print_board()
+def game():
+    place = {'1': (0, 0), '2': (0, 1), '3': (0, 2),
+             '4': (1, 0), '5': (1, 1), '6': (1, 2),
+             '7': (2, 0), '8': (2, 1), '9': (2, 2)}
+    turn = 'X'
+    count = 0
+
+    board = Board()
+    for i in range(10):
+        board.print_board()
+        print("turn:" + turn + " Choose a place?")
+        move = input()
+        x, y = place.get(move, (-1, -1))
+        if x == y != -1 and board.matrix[x][y].click:
+            board.matrix[x][y].value = turn
+            board.matrix[x][y].click = False
+            count += 1
+        else:
+            print('Wrong Place choose again!')
+            i -= 1
+            continue
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
